478a1eaee7404d00cb9d5b8dbad485e0f6c2da93
Niklas Björnestål <niklas.bjornestal@hotmail.se>
2019-09-10 20:40:57 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #![feature(proc_macro_hygiene, decl_macro)]
-
 #[macro_use] extern crate rocket;
 use rocket::config::{Config, Environment};
-use std::io;
 use std::env;
 
+/**
+ * Add route to rocket webserver that calculates mandelbrot fractal and return it as byte array (8-bit grayscale)
+ */
 #[get("/<min_c_re>/<min_c_im>/<max_c_re>/<max_c_im>/<xsize>/<ysize>/<inf_n>")]
 fn mandelbrot(min_c_re :f32, min_c_im :f32, max_c_re :f32, max_c_im :f32, xsize :u32 ,ysize :u32, inf_n :u32) -> Vec<u8> {
     let mut image: Vec<u8> = Vec::new();
@@ -12,12 +13,12 @@ fn mandelbrot(min_c_re :f32, min_c_im :f32, max_c_re :f32, max_c_im :f32, xsize 
 		for x in 0..xsize {
 			let mut zx = 0.0;
 			let mut zy = 0.0;
-			let cX = x as f32 * (max_c_re - min_c_re) / xsize as f32 + min_c_re;
-			let cY = y as f32 * (max_c_im - min_c_im) / ysize as f32 + min_c_im;
+			let c_x = x as f32 * (max_c_re - min_c_re) / xsize as f32 + min_c_re;
+			let c_y = y as f32 * (max_c_im - min_c_im) / ysize as f32 + min_c_im;
             let mut iter = inf_n;
-			while (zx * zx + zy * zy < 4.0 && iter > 0) {
-				let tmp = zx * zx - zy * zy + cX;
-				zy = 2.0 * zx * zy + cY;
+			while zx * zx + zy * zy < 4.0 && iter > 0 {
+				let tmp = zx * zx - zy * zy + c_x;
+				zy = 2.0 * zx * zy + c_y;
 				zx = tmp;
 				iter -= 1;
 			}
@@ -32,6 +33,7 @@ fn main() -> Result<(), rocket::config::ConfigError> {
 
 	if args.len() != 3 {
 		eprintln!("Usage:\n{} server port", args[0]);
+		return Ok(())
 	}
 	let server = &args[1];
 	let port = args[2].parse().unwrap();
@@ -44,6 +46,5 @@ fn main() -> Result<(), rocket::config::ConfigError> {
     rocket::custom(config)
         .mount("/mandelbrot", routes![mandelbrot])
         .launch();
-//	rocket::ignite().mount("/hello", routes![hello]).launch();
 	Ok(())
 }
